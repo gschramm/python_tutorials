64c67a1f8aaccde727ac5ce0cdfd1eda39b59fed
Georg Schramm <georg.schramm@posteo.net>
2022-07-16 10:46:31 +0200

    use pep8 style

--- recon/mlem_sens_img.py
+++ recon/mlem_sens_img.py
@@ -6,31 +6,37 @@ import matplotlib.pyplot as plt
 
 from typing import Optional, Union
 
+
 class AcqModel:
     def __init__(self, A: np.ndarray) -> None:
-        self.A = A  
+        self.A = A
+
     def forward(self, x: np.ndarray) -> np.ndarray:
         return self.A @ x
+
     def adjoint(self, y: np.ndarray) -> np.ndarray:
         return self.A.T @ y
 
+
 class MLEM:
-    def __init__(self, acq_model: AcqModel, 
-                       data: np.ndarray, 
-                       contamination: Optional[np.ndarray] = None, 
-                       scalar_sens_image: Optional[bool] = False) -> None:
-        self.acq_model     = acq_model
-        self.data          = data
+    def __init__(self,
+                 acq_model: AcqModel,
+                 data: np.ndarray,
+                 contamination: Optional[np.ndarray] = None,
+                 scalar_sens_image: Optional[bool] = False) -> None:
+        self.acq_model = acq_model
+        self.data = data
         self.contamination = contamination
 
         # init recon
         self.init_image()
 
         # calc. sens image
-        self.adjoint_ones = self.acq_model.adjoint(np.ones(self.acq_model.A.shape[0]))
+        self.adjoint_ones = self.acq_model.adjoint(
+            np.ones(self.acq_model.A.shape[0]))
 
         if scalar_sens_image:
-          self.adjoint_ones = 0*self.adjoint_ones + self.adjoint_ones.max()
+            self.adjoint_ones = 0 * self.adjoint_ones + self.adjoint_ones.max()
 
         # init iteration counter
         self.iteration_counter = 0
@@ -38,7 +44,7 @@ class MLEM:
     def init_image(self) -> None:
         # initialize recon with ones
         self.image = np.ones(self.acq_model.A.shape[1])
-        self.iterates:list  = [] 
+        self.iterates: list = []
 
     def update(self) -> None:
         exp = self.acq_model.forward(self.image)
@@ -46,10 +52,10 @@ class MLEM:
         if self.contamination is not None:
             exp += self.contamination
 
-        gradient = self.acq_model.adjoint((self.data - exp)/exp)
-        step     = self.image / self.adjoint_ones
+        gradient = self.acq_model.adjoint((self.data - exp) / exp)
+        step = self.image / self.adjoint_ones
 
-        self.image += step*gradient
+        self.image += step * gradient
 
         self.iterates.append(self.image.copy())
 
@@ -60,48 +66,57 @@ class MLEM:
 
         return np.array(self.iterates)
 
+
 #----------------------------------------------------------------------------------------------
 np.random.seed(1)
 
-niter  = 1000
-A      = np.random.rand(5,3)
-A[:,1] *= 5
-x_true = 10*np.random.rand(A.shape[1])
+niter = 1000
+A = np.random.rand(5, 3)
+A[:, 1] *= 5
+x_true = 10 * np.random.rand(A.shape[1])
 #----------------------------------------------------------------------------------------------
 
 model = AcqModel(A)
 
 noise_free_data = model.forward(x_true)
-contam          = np.full(noise_free_data.shape, 5.3) 
+contam = np.full(noise_free_data.shape, 5.3)
 
 noise_free_data += contam
 
-reconstructor_1 = MLEM(model, noise_free_data, contamination = contam, scalar_sens_image = False)
-reconstructor_2 = MLEM(model, noise_free_data, contamination = contam, scalar_sens_image = True)
+reconstructor_1 = MLEM(model,
+                       noise_free_data,
+                       contamination=contam,
+                       scalar_sens_image=False)
+reconstructor_2 = MLEM(model,
+                       noise_free_data,
+                       contamination=contam,
+                       scalar_sens_image=True)
 
 x1 = reconstructor_1.run(niter)
 x2 = reconstructor_2.run(niter)
 
-
 #----------------------------------------------------------------------------------------------
 # show the solutions
-fig, ax = plt.subplots(2, x_true.shape[0], figsize = (3*x_true.shape[0],6), sharey = 'row')
+fig, ax = plt.subplots(2,
+                       x_true.shape[0],
+                       figsize=(3 * x_true.shape[0], 6),
+                       sharey='row')
 
 for i in range(x_true.shape[0]):
-    ax[0,i].plot(x1[:,i])
-    ax[0,i].plot(x2[:,i])
-    ax[0,i].axhline(x_true[i], color = 'k')
-    ax[0,i].grid(ls = ':')
+    ax[0, i].plot(x1[:, i])
+    ax[0, i].plot(x2[:, i])
+    ax[0, i].axhline(x_true[i], color='k')
+    ax[0, i].grid(ls=':')
 
-    ax[1,i].loglog(np.abs(x1[:,i] - x_true[i]))
-    ax[1,i].loglog(np.abs(x2[:,i] - x_true[i]))
-    ax[1,i].grid(ls = ':')
+    ax[1, i].loglog(np.abs(x1[:, i] - x_true[i]))
+    ax[1, i].loglog(np.abs(x2[:, i] - x_true[i]))
+    ax[1, i].grid(ls=':')
 
-    ax[0,i].set_xlabel('iteration')
-    ax[1,i].set_xlabel('iteration')
+    ax[0, i].set_xlabel('iteration')
+    ax[1, i].set_xlabel('iteration')
 
-ax[0,0].set_ylabel('recon. value')
-ax[1,0].set_ylabel('abs(recon value - g.t.)')
+ax[0, 0].set_ylabel('recon. value')
+ax[1, 0].set_ylabel('abs(recon value - g.t.)')
 
 fig.tight_layout()
 fig.show()
