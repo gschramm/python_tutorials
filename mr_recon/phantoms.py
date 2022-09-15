577939455f9e82d1ac0f271f35f1b5b72a1cf586
Georg Schramm <gschra2@stanford.edu>
2022-09-14 22:12:51 -0700

    add simple rod phantom

---
+++ mr_recon/phantoms.py
@@ -0,0 +1,58 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def rod_phantom(n: int = 256,
+                r: float = 0.9,
+                r_rod: float = 0.08,
+                nrods: int = 5,
+                rod_contrast: None | npt.NDArray = None) -> npt.NDArray:
+    """3D cylinder rod phantom
+
+    Parameters
+    ----------
+    n : int, optional
+        spatial dimension, by default 256
+    r : float, optional
+        relative radius of cylinder, by default 0.9
+    r_rod : float, optional
+        reltative radius of rods , by default 0.08
+    nrods : int, optional
+        number of rods, by default 5
+    rod_contrast : None | npt.NDArray, optional
+        rod contrast, by default None
+
+    Returns
+    -------
+    npt.NDArray
+        the image array of shape (n,n,n)
+    """
+
+    if rod_contrast is None:
+        rod_contrast = np.linspace(0.1, 1.2, nrods)
+
+    x = np.linspace(-1, 1, n)
+    X, Y = np.meshgrid(x, x, indexing='ij')
+
+    R = np.sqrt((X - 0)**2 + (Y - 0)**2)
+
+    x = np.zeros((n, n))
+
+    x[R < r] = 1
+
+    for i, ox in enumerate(
+            np.linspace(-0.8 * r / np.sqrt(2), 0.8 * r / np.sqrt(2), nrods)):
+        for j, oy in enumerate(
+                np.linspace(-0.8 * r / np.sqrt(2), 0.8 * r / np.sqrt(2),
+                            nrods)):
+            R = np.sqrt((X - ox)**2 + (Y - oy)**2)
+            x[R < r_rod] = rod_contrast[i]
+
+    # repeat 2D arrays into 3D array
+    x = np.repeat(x[:, :, np.newaxis], n, axis=2)
+
+    # delete first / last slices
+    x[:, :, :(n // 8)] = 0
+    x[:, :, ((7 * n) // 8):] = 0
+
+    return x
