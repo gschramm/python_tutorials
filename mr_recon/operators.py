correct adjoint of complex gradient operator

mr_recon/operators.py
@@ -678,12 +678,12 @@ class ComplexGradientOperator(LinearOperator):
 
         for i in range(self._ndim):
             sl = i * (self._sl_all, ) + (
-                self._sl_first, ) + (self._ndim - i - 1) * (self._sl_all, )
-            d[..., 0] -= np.diff(y[i, ..., 0],
-                                 axis=i,
-                                 prepend=y[i, ..., 0][sl])
-            d[..., 1] -= np.diff(y[i, ..., 1],
-                                 axis=i,
-                                 prepend=y[i, ..., 1][sl])
+                self._sl_last, ) + (self._ndim - i - 1) * (self._sl_all, )
+            tmp0 = y[i, ..., 0]
+            tmp0[sl] = 0
+            d[..., 0] -= np.diff(tmp0, axis=i, prepend=0)
+            tmp1 = y[i, ..., 1]
+            tmp1[sl] = 0
+            d[..., 1] -= np.diff(tmp1, axis=i, prepend=0)
 
         return d

mr_recon/recon_differentiable_cost_stack_of_stars.py
@@ -14,10 +14,10 @@ import matplotlib.pyplot as plt
 n: int = 128
 n2: int = 15
 num_channels: int = 1
-noise_level: float = 0.1
+noise_level: float = 0.
 seed: int = 0
 
-num_spokes: int = 32
+num_spokes: int = 10
 num_samples_per_spoke: int = 2 * n - 1
 
 num_iter: int = 200
@@ -25,7 +25,7 @@ num_iter: int = 200
 data_norm = L2NormSquared()
 
 prior_norm = L2NormSquared()
-beta: float = 1e2
+beta: float = 0
 
 #----------------------------------------------------------------------------------------
 np.random.seed(seed)