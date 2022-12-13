ab632b0a186c38a3cba52d1f90a53969180a8823
Georg <gschra2@stanford.edu>
2022-12-13 14:23:57 -0800

    add readout factor

--- kspace_trajetories/test.py
+++ kspace_trajetories/test.py
@@ -7,8 +7,8 @@ from operators import FFT, GradientOperator
 from algorithms import PDHG
 
 
-def t_of_k(k):
-    return 40 * np.abs(k) / 0.91391
+def t_of_k(k, factor: float = 1.):
+    return factor * 40 * np.abs(k) / 0.91391
 
 
 def mse(x, y):
@@ -24,6 +24,7 @@ def mae(x, y):
 if __name__ == '__main__':
 
     xp = np
+    xp.random.seed(1)
 
     n = 128
     x0 = 110.
@@ -31,10 +32,15 @@ if __name__ == '__main__':
     num_iter = 2000
     rho = 1e2
     prior = 'L1L2Norm'
-    betas = [1e0, 3e0, 1e1, 3e1]
+    betas = np.logspace(-1, 1.5, 9)
     #prior = 'SquaredL2Norm'
     #betas = [1e1, 1e2, 1e3]
-    T2star_factor = 1000.
+    T2star_factor = 1.
+    readout_time_factor = 1. / 4
+
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
 
     signal_csf1 = SquareSignal(stretch=20. / x0,
                                scale=1,
@@ -84,13 +90,21 @@ if __name__ == '__main__':
     #-----------------------------------------------------------------------------------------------------------------
 
     noise_free_data = xp.zeros(n, dtype=xp.complex128)
+    t_readout = xp.zeros(n)
 
     for i, kk in enumerate(k):
-        noise_free_data[i] = signal.continous_ft(kk, t=t_of_k(kk))
+        t_r = t_of_k(kk, factor=readout_time_factor)
+        t_readout[i] = t_r
+        noise_free_data[i] = signal.continous_ft(kk, t=t_r)
 
-    data = noise_free_data.copy() + noise_level * xp.random.randn(
+    scaled_noise_level = noise_level / np.sqrt(readout_time_factor)
+
+    data = noise_free_data.copy() + scaled_noise_level * xp.random.randn(
         *noise_free_data.shape) + 1j * noise_level * xp.random.randn(
             *noise_free_data.shape)
+
+    print(f'readout time factor .: {readout_time_factor:.1e}')
+    print(f'scaled noise level  .: {scaled_noise_level:.1e}')
 
     #-----------------------------------------------------------------------------------------------------------------
     #-----------------------------------------------------------------------------------------------------------------
@@ -132,11 +146,13 @@ if __name__ == '__main__':
     #-----------------------------------------------------------------------------------------------------------------
     #-----------------------------------------------------------------------------------------------------------------
 
-    recon_mse = [mse(recon, signal.signal(x)) for recon in pdhg_recons]
-    recon_mae = [mae(recon, signal.signal(x)) for recon in pdhg_recons]
+    metrics_dict = dict(MSE=mse, MAE=mae)
+    results = {}
 
-    print("global MSE", recon_mse)
-    print("global MAE", recon_mae)
+    for key, metric in metrics_dict.items():
+        results[key] = [
+            metric(recon, signal.signal(x)) for recon in pdhg_recons
+        ]
 
     #-----------------------------------------------------------------------------------------------------------------
     #-----------------------------------------------------------------------------------------------------------------
@@ -144,13 +160,11 @@ if __name__ == '__main__':
     #-----------------------------------------------------------------------------------------------------------------
     #-----------------------------------------------------------------------------------------------------------------
 
-    print(rho, pdhg_costs[:, -1])
-
     xx = xp.linspace(-x0, x0, 1000, endpoint=False)
     kk = xp.linspace(k.min(), k.max(), 1000, endpoint=False)
     it = np.arange(1, num_iter + 1)
 
-    fig, ax = plt.subplots(1, 4, figsize=(16, 4))
+    fig, ax = plt.subplots(1, 5, figsize=(16, 4))
     ax[0].plot(xx, signal.signal(xx).real, 'k-', lw=0.5)
     ax[1].plot(xx, signal.signal(xx).imag, 'k-', lw=0.5)
     ax[0].plot(x, recon1.real, '-', lw=0.8)
@@ -175,5 +189,39 @@ if __name__ == '__main__':
     ax[1].set_ylim(*ax[0].get_ylim())
     ax[1].legend()
 
+    ax[4].plot(k, t_readout, '.')
+
     fig.tight_layout()
     fig.show()
+
+    #-----------------------------------------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------------------------------------------
+    # plot metrics
+    #-----------------------------------------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------------------------------------------
+
+    fig2, ax2 = plt.subplots(3,
+                             len(metrics_dict),
+                             figsize=(3 * len(metrics_dict), 3 * 3))
+    i = 0
+    for key, metric in results.items():
+        imin = np.argmin(metric)
+        print(
+            f'{key}:   opt.beta: {betas[imin]:.2e}   opt.value: {metric[imin]:.2e}'
+        )
+        ax2[0, i].loglog(betas, metric, 'x-')
+        ax2[0, i].loglog([betas[imin]], [metric[imin]], 'x')
+        ax2[0, i].set_xlabel('beta')
+        ax2[0, i].set_ylabel(key)
+        ax2[1, i].plot(xx, signal.signal(xx).real, 'k-', lw=0.5)
+        ax2[1, i].plot(x, pdhg_recons[imin, :].real, '-', lw=0.8)
+        ax2[2, i].plot(xx, signal.signal(xx).imag, 'k-', lw=0.5)
+        ax2[2, i].plot(x, pdhg_recons[imin, :].imag, '-', lw=0.8)
+        ax2[0, i].set_title(key)
+        i += 1
+
+    for axx in ax2.ravel():
+        axx.grid(ls=':')
+
+    fig2.tight_layout()
+    fig2.show()
